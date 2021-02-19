d99ced55497904197e89f8b7520f4d447c79de28
Ryan Mohta <ryanmohta@gmail.com>
2021-02-19 11:20:03 -0800

    Updated indentation of html

--- client/src/Home.js
+++ client/src/Home.js
@@ -17,14 +17,14 @@ class Home extends React.Component {
       //score : null,
       //description: null
     };
-  
+
     this.getSelectedValue1 = this.getSelectedValue1.bind(this);
 
 
     // new button
     this.onButtonPressed1=this.onButtonPressed1.bind(this);
   }
-  
+
 
   handleChange = selectedChoice => {
     this.setState({ selectedChoice });
@@ -37,7 +37,7 @@ class Home extends React.Component {
     console.log(sign1);
   }
 
-  
+
 
   onButtonPressed1() {
     this.props.history.push('/login');
@@ -46,50 +46,43 @@ class Home extends React.Component {
 
   render() {
     return(
-      
       <div id="app">
-      <ParticlesBg type="circle" bg={true} />
-<img src={logo} alt="Logo" />
-<div>
-  <form class="pure-form pure-form-aligned">
-    <fieldset>
-      <div class="pure-control-group">
-        <input id="name" type="text" placeholder="Username" onChange={this.getSelectedValue1}/>
+        <ParticlesBg type="circle" bg={true} />
+        <img src={logo} alt="Logo" />
+        <div>
+          <form class="pure-form pure-form-aligned">
+            <fieldset>
+              <div class="pure-control-group">
+                <input id="name" type="text" placeholder="Username" onChange={this.getSelectedValue1}/>
+              </div>
+              <div class="pure-control-group">
+                <input
+                  class="pure-input-1-2"
+                  id="password"
+                  type="password"
+                  id="password"
+                  type="password"
+                  class="pure-input-1-2"
+                  id="password"
+                  type="password"
+                  placeholder="Password"
+                />
+              </div>
+              <Button variant="warning" class="mx-auto" onClick={this.onButtonPressed1}>Login</Button>
+            </fieldset>
+          </form>
+        </div>
       </div>
 
-      <div class="pure-control-group">
-        <input
-          class="pure-input-1-2"
-          id="password"
-          type="password"
-          id="password"
-          type="password"
-          class="pure-input-1-2"
-          id="password"
-          type="password"
-          placeholder="Password"
-        />
-      </div>
-      <Button variant="warning" class="mx-auto" onClick={this.onButtonPressed1}>Login</Button>
-
-    </fieldset>
-  </form>
- 
-</div>   
-</div>  
-
-
-  
-        
       );
     }
-  
-        
-    
-  
-        
-    
+
+
+
+
+
+
     }
-  
-  
+
+
 export default Home;
--- client/src/login.js
+++ client/src/login.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import {Button} from "react-bootstrap";
 import {useHistory} from "react-router-dom"
-//import 'bootstrap/dist/css/bootstrap.min.css';
 import ParticlesBg from 'particles-bg'
 
-
-
 import "./login.css";
-
 
 class Login extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selectedA: null 
+      selectedA: null
     };
 
     this.onButtonPressed1=this.onButtonPressed1.bind(this);
@@ -29,32 +25,22 @@ class Login extends React.Component {
   onButtonPressed2() {
     this.props.history.push('/receive');
   }
-  
+
     render() {
         return (
-          <div> <a id= 'Button' class="btn btn-primary" role="button" href='./'>Back</a> 
-    
+          <div>
+            <a id= 'Button' class="btn btn-primary" role="button" href='./'>Back</a>
             <div id="app">
-              
-                <ParticlesBg type="circle" bg={true} />
-      <h1>Welcome {this.props.name}</h1>
-      <h2>Would you like to send or receive a gram?</h2>
-      <h2>All fundings will go to charity</h2>
-      <div>
-        <form class="pure-form pure-form-aligned">
-        <a id= 'Send' class="btn btn-primary" role="button" href='/send'>Send</a>
-        <a id = 'Receive' class="btn btn-primary" role="button" href='/car'>Receive</a>
-        
-
-        </form>
-
-
-      </div>
-      
-    </div>
-    </div>
-    
-    
+              <ParticlesBg type="circle" bg={true} />
+              <h1>Welcome {this.props.name}</h1>
+              <h2>Would you like to send or receive a gram?</h2>
+              <h2>All fundings will go to charity</h2>
+            <div>
+            <form class="pure-form pure-form-aligned">
+              <a id= 'Send' class="btn btn-primary" role="button" href='/send'>Send</a>
+              <a id = 'Receive' class="btn btn-primary" role="button" href='/car'>Receive</a>
+            </form>
+          </div>
         )
     }
   }
