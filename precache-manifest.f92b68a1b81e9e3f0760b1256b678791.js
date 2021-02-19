23e78e8a0eb66b6249fd12bc4f4888c587430703
Jennie Quach <49262417+quachjennifer@users.noreply.github.com>
2021-02-19 12:53:40 -0800

    Updates

---
+++ precache-manifest.f92b68a1b81e9e3f0760b1256b678791.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14632aea2b3050b8aa571291c0108302",
+    "url": "/Me2U/index.html"
+  },
+  {
+    "revision": "94188f25760166a634bc",
+    "url": "/Me2U/static/css/main.e0afd635.chunk.css"
+  },
+  {
+    "revision": "857ee2df5df10b39f4e3",
+    "url": "/Me2U/static/js/2.9ff97276.chunk.js"
+  },
+  {
+    "revision": "a8b15fcc633b7864f2dc19d56211bd16",
+    "url": "/Me2U/static/js/2.9ff97276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94188f25760166a634bc",
+    "url": "/Me2U/static/js/main.b99f643b.chunk.js"
+  },
+  {
+    "revision": "1d3ed5d2a8ab0e7f4ff4",
+    "url": "/Me2U/static/js/runtime-main.e3102cb4.js"
+  },
+  {
+    "revision": "793529bece58dc90150a1d4cb694840a",
+    "url": "/Me2U/static/media/Logo.793529be.png"
+  }
+]);
