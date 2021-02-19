cd1e1ac8513d88e1fa9e62983b11aa8515ea6c92
Ryan Mohta <ryanmohta@gmail.com>
2021-02-19 11:25:56 -0800

    Indentation bug fix

--- client/src/login.js
+++ client/src/login.js
@@ -35,11 +35,13 @@ class Login extends React.Component {
               <h1>Welcome {this.props.name}</h1>
               <h2>Would you like to send or receive a gram?</h2>
               <h2>All fundings will go to charity</h2>
-            <div>
-            <form class="pure-form pure-form-aligned">
-              <a id= 'Send' class="btn btn-primary" role="button" href='/send'>Send</a>
-              <a id = 'Receive' class="btn btn-primary" role="button" href='/car'>Receive</a>
-            </form>
+              <div>
+                <form class="pure-form pure-form-aligned">
+                  <a id= 'Send' class="btn btn-primary" role="button" href='/send'>Send</a>
+                  <a id = 'Receive' class="btn btn-primary" role="button" href='/car'>Receive</a>
+                </form>
+              </div>
+            </div>
           </div>
         )
     }
